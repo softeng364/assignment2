10d2806157f89f443afae84777231fcac74ff8f4
Jon Pearce <jonpea@gmail.com>
2018-05-17 10:09:59 +1200

    Added tests

--- checksum.py
+++ checksum.py
@@ -7,6 +7,4 @@ def internet_checksum(data, total=0x0):
     1. https://tools.ietf.org/html/rfc1071
     2. http://www.netfor2.com/checksum.html
     '''
-	pass
-
-	
+	pass # TODO: Implement this function and return the checksum

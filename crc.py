22f0d568c0eba492db3ea7b2e244c441cc38adb1
Jon Pearce <jonpea@gmail.com>
2018-05-17 10:10:47 +1200

    Initial commit

---
+++ crc.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from bitarray import bitarray
+
+def xor_at(a, b, offset=0):
+    for k, bk in enumerate(b):
+        index = offset + k
+        a[index] = a[index] ^ bk
+
+def crc(d, g):
+	# We'd prefer not to modify the argument in xor_at
+    dcopy = d.copy()  
+	#
+	# TODO: compute and return remainder "r"
+	#
+
+
+if __name__ == '__main__':
+
+    print("From Kurose & Ross (7e), page 478:")
+    g = bitarray('1001')
+    d = bitarray('101110000')
+    r = crc(d, g)
+    assert r == bitarray('011')
+    
+    print("From Wikipedia, [en.wikipedia.org/wiki/Cyclic_redundancy_check]:")
+    g = bitarray('1011')
+    d = bitarray('11010011101100000')
+    r = crc(d, g)
+    assert r == bitarray('100')
+    

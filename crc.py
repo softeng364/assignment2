4d748c26c1b18a038ce07761f3b99c2a0aeadab7
Jon Pearce <jonpea@gmail.com>
2018-05-17 18:06:07 +1200

    Clarified whether or not data should be shifted/padded

--- crc.py
+++ crc.py
@@ -17,14 +17,18 @@ def crc(d, g):
 if __name__ == '__main__':
 
     print("From Kurose & Ross (7e), page 478:")
-    g = bitarray('1001')
-    d = bitarray('101110000')
-    r = crc(d, g)
-    assert r == bitarray('011')
+    g = bitarray('1001')            # generator
+    d = bitarray('101110')          # data (without padding/shifting)
+    p = bitarray('000')             # padding
+    r = crc(d + p, g)               # error-correction bits
+    assert r == bitarray('011')     # known quotient
+    assert crc(d + r, g) == p       # perform CRC check
     
     print("From Wikipedia, [en.wikipedia.org/wiki/Cyclic_redundancy_check]:")
-    g = bitarray('1011')
-    d = bitarray('11010011101100000')
-    r = crc(d, g)
-    assert r == bitarray('100')
+    g = bitarray('1011')            # generator
+    d = bitarray('11010011101100')  # data (without padding/shifting) 
+    p = bitarray('000')             # padding
+    r = crc(d + p, g)               # error-correction bits
+    assert r == bitarray('100')     # known quotient
+    assert crc(d + r, g) == p       # perform CRC check
     
